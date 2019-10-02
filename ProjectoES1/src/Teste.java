fee8d90c2834301ac98a0fd323b474793d325eb0
fmupa-iscteiul <fmupa@iscte-iul.pt>
2019-10-02 16:16:19 +0100

    first push

--- ProjectoES1/src/Teste.java
+++ ProjectoES1/src/Teste.java
@@ -3,7 +3,7 @@ public class Teste {
 	
 	public static void main(String[] args) {
 		System.out.println("está a funcionar");
-		System.out.println("teste");
+		System.out.println("testeee");
 	}
 
 }
