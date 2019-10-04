e1495e816779c387e4d88a11ca549488b7ef6dfc
Sene Conté <sceee@iscte-iul.pt>
2019-10-04 10:02:08 +0100

    função sen(x)/x

--- ProjectoES1/src/Teste.java
+++ ProjectoES1/src/Teste.java
@@ -1,9 +1,14 @@
 
 public class Teste {
 	
+	static double f(double x) {
+		return (Math.sin(x))/x;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("está a funcionar");
 		System.out.println("teste");
+		System.out.println(f(0.0000001));
 	}
 
 }
